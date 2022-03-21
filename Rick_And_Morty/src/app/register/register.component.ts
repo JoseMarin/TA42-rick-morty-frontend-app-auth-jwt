493421bcc57a8cc95c0ddf05de947f2cfd471bbb
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-21 20:04:28 +0100

    métodos del register

--- Rick_And_Morty/src/app/register/register.component.ts
+++ Rick_And_Morty/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+//IMPORTACIÓN DEL SEVICE AUTHENTICACION
+import { AuthService } from '../_services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  //ATRIBUTOS DEL FORM
+  form : any = {
+    username : null,
+    email : null,
+    password : null
+  };
+
+  //ESTADOS A VERIFICAR DESDE EL FORM
+  esSatisfactorio = false;
+  esFallido = false;
+  errorMessage = '';
+
+  constructor(private authServices : AuthService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() : void {
+    const { username, email, password } = this.form;
+
+    this.authServices.register(username, email, password)
+      .subscribe( data => {
+        console.log(data);
+        this.esSatisfactorio = true;
+        this.esFallido = false;
+      },
+      error => {
+        this.errorMessage = error.error.errorMessage;
+        this.esFallido = true;
+      }
+      );
+  }
 }
