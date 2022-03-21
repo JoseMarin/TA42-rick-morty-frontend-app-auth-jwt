ded92bb7ce4f3f0b233a673f0a152b421af2bf53
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-21 17:43:44 +0100

    agregado de paths por compoenntes nuevos

--- Rick_And_Morty/src/app/_services/token-storage.service.ts
+++ Rick_And_Morty/src/app/_services/token-storage.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
 
+//CONSTANTES DEL STORAGE
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorageService {
 
   constructor() { }
+
+  salir() : void {
+    window.sessionStorage.clear();
+  }
+
+  public guardaToken(token : string) : void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public obtenToken() : string | null {
+    return window.sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public guardaUser(user : any) : void{
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public ubicaUser() : any {
+    const user = window.sessionStorage.getItem(USER_KEY);
+    if(user) {
+      return JSON.parse(user);
+    }
+
+    return {};
+  }
 }
--- Rick_And_Morty/src/app/app-routing.module.ts
+++ Rick_And_Morty/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
 import { BusquedaComponent } from './formulario/busqueda/busqueda.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { LoginComponent } from './login/login.component';
 import { PersonajeComponent } from './personajes/personaje/personaje.component';
 import { PersonajesComponent } from './personajes/personajes.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 //DEFINICION DE LOS PATHS A APUNTAR PARA GENERAR LA DINÁMICA DE LA PÁGINA
 // LA DEFINICIÓN DE LAOS PATHS SE INDEXAN EN FORMATO JSON
@@ -28,6 +34,30 @@ const routes: Routes = [
   {
     path : 'busqueda',
     component : BusquedaComponent // INCLUSION DE NUEVO COMPONENT BÚSQUEDA
+  },
+  {
+    path : 'login',
+    component : LoginComponent
+  },
+  {
+    path : 'register',
+    component : RegisterComponent
+  },
+  {
+    path : 'profile',
+    component : ProfileComponent
+  },
+  {
+    path : 'user',
+    component : BoardUserComponent
+  },
+  {
+    path : 'mod',
+    component : BoardModeratorComponent
+  },
+  {
+    path : 'admin',
+    component : BoardAdminComponent
   }
 ];
 
