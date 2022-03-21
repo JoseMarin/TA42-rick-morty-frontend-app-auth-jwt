2519d22c25c0c2208e34d725354d5cc37d68b87f
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-21 19:50:43 +0100

    edicion del main

--- Rick_And_Morty/src/app/app.component.ts
+++ Rick_And_Morty/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Rick_And_Morty';
+  private roles : string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username? : string;
+
+  constructor(private tokenStorageService : TokenStorageService) {} //INYECCION DEL TOKENSTORAGE
+
+  ngOnInit() : void {
+    //DESDE ONINIT SE DISPARA LOS ROLES Y LA ADMISIÓN A RECURSOS DE LOS MISMOS
+    this.isLoggedIn = !!this.tokenStorageService.obtenToken();
+
+    if(this.isLoggedIn) {
+      const user = this.tokenStorageService.ubicaUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout() : void {
+    this.tokenStorageService.salir();
+    window.location.reload();
+  }
 }
