0235e9e69753ce0d461a6a1246a5a34848a1a456
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-21 17:11:40 +0100

    edición de los services de login y register

--- Rick_And_Morty/src/app/_services/auth.service.ts
+++ Rick_And_Morty/src/app/_services/auth.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+const AUTH_API = '';
+const httpOptions = {
+  headers : new HttpHeaders({ 'Content-Type' : 'application/json' })
+}; //HEADER DECLARADO COMO CONSTANTE
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AuthService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { } //INYECCIÓN DEL HTTPCLIENT
+
+  //MÉTODO PARA LOGIN
+  login (username : string, password : string) : Observable <any> {
+    return this.http.post( AUTH_API + 'signin' , {
+      username,
+      password
+    }, httpOptions);
+  }
+
+  //MÉTODO DE REGISTROS, TOMARÁ TODOS LOS VALORES QUE ALLÍ SE LE INDIQUEN
+  register(username : string, email : string, password : string) : Observable <any> {
+    return this.http.post(AUTH_API + 'signup' , {
+      username,
+      email,
+      password
+    }, httpOptions);
+  }
 }
